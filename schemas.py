b66679e980d61020923cb2df39f0c5f4bea4060f
Brian Russell <brianrussell@github.com>
2023-08-10 15:56:06 +0100

    finished sql

--- schemas.py
+++ schemas.py
@@ -1,10 +1,5 @@
 from marshmallow import Schema, fields
-
-class PlainTagSchema(Schema):
-    id = fields.Int(dump_only=True)
-    name = fields.Str()
-
-    
+   
 # Omitting store id avoids recursive inclusion
 class PlainItemSchema(Schema):
     id = fields.Int(dump_only=True)
@@ -14,23 +9,36 @@ class PlainItemSchema(Schema):
 class PlainStoreSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
+ 
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)         # Many to Many
+    
+    
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)       # Many to Many
+    
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+class TagAndItemSchema(Schema):                                                 # Many to many
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
+    
+class StoreUpdateSchema(Schema):
+    name = fields.Str(required=True)
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
     store_id = fields.Int()
-    
-class StoreUpdateSchema(Schema):
-    name = fields.Str(required=True)
 
-class ItemSchema(PlainItemSchema):
-    store_id = fields.Int(required=True, load_only=True)
-    store = fields.Nested(PlainStoreSchema(), dump_only=True)
-    
-class StoreSchema(PlainStoreSchema):
-    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
-    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
-    
-class TagSchema(PlainTagSchema):
-    store_id = fields.Int(load_only=True)
-    store = fields.Nested(PlainStoreSchema(), dump_only=True)
