714a76c852f4100761abc8b2123d059aac2aa3eb
Brian Russell <brianrussell@github.com>
2023-08-16 10:16:12 +0100

    added gunicorn

--- runme.sh
+++ runme.sh
@@ -2,6 +2,6 @@
 
 # Run docker and mount dev folder so you can edit and keep the container running
 
-docker run -dp 5000:5000 -w /app -v "$(pwd):/app" --name app flaskapi
+docker run -dp 5000:5000 -w /app -v "$(pwd):/app" --name app flaskapi sh -c "flask run --host 0.0.0.0"
 
 docker logs -f --tail 10 app
