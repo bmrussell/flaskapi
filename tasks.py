874c862230fe1e06b7b47c54f37b6f3adbee3863
Brian Russell <brianrussell@github.com>
2023-08-22 11:59:48 +0100

    Jinja2 mail template and worker settings

--- tasks.py
+++ tasks.py
@@ -1,20 +1,35 @@
 from utils import getenv
 import requests
-import rq
+import jinja2
+
+template_loader = jinja2.FileSystemLoader("templates")
+template_env = jinja2.Environment(loader=template_loader)
 
 
-def send_simple_message(to, subject, body):
+def render_template(filename, **context):
+    return template_env.get_template(filename).render(**context)
+
+def send_simple_message(to, subject, body, html):
     domain = getenv("MAILGUN_DOMAIN")
     key = getenv("MAILGUN_API_KEY")
 
     return requests.post(
         f"https://api.mailgun.net/v3/{domain}/messages",
         auth=("api", key),
-        data={"from": f"App <mailgun@{domain}>",
-              "to": [to],
-              "subject": subject,
-              "text": body})
+        data={
+            "from": f"App <mailgun@{domain}>",
+            "to": [to],
+            "subject": subject,
+            "text": body,
+            "html": html,
+        },
+    )
 
 
 def send_user_registration_email(email, username):
-    return send_simple_message(email, "Successfully signed up", f"Hi {username}! You have sucessfully signed up to the Stores REST API.")
+    return send_simple_message(
+        email,
+        "Successfully signed up",
+        f"Hi {username}! You have sucessfully signed up to the Stores REST API.",
+        render_template("email/action.html", username=username)
+    )
---
+++ workersettings.py
@@ -0,0 +1,5 @@
+import utils
+
+REDIS_URL = utils.getenv("REDIS_URL")
+QUEUES = {"emails", "default"}
+
