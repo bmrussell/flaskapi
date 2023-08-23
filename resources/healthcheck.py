360ef664472db7fce62bc83cd8f4d8a66a41ceea
Brian Russell <brianrussell@github.com>
2023-08-23 10:48:16 +0100

    fixed function name

--- resources/healthcheck.py
+++ resources/healthcheck.py
@@ -1,13 +1,16 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
+
 blp = Blueprint("Healthchecks", __name__, description="Service Healthchecks")
 
 ###################################################################################
 # /item/<int:id>
 ###################################################################################
+
+
 @blp.route("/health")
-class Item(MethodView):
+class Health(MethodView):
     @blp.response(200)
-    def get(self):        
+    def get(self):
         return {"message": "OK"}
